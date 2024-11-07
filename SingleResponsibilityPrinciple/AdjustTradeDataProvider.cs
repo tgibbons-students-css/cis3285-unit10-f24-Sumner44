34e66bdac4cc8f2394e722e659715d4aa90ff565
Sumner Cotton <scotton@css.edu>
2024-11-07 10:56:39 -0600

    Code for learning activity

---
+++ SingleResponsibilityPrinciple/AdjustTradeDataProvider.cs
@@ -0,0 +1,40 @@
+﻿using SingleResponsibilityPrinciple.Contracts;
+using System;
+using System.Collections.Generic;
+
+namespace SingleResponsibilityPrinciple
+{
+    public class AdjustTradeDataProvider : ITradeDataProvider
+    {
+        ITradeDataProvider originProvider;
+
+     
+        public AdjustTradeDataProvider(ITradeDataProvider originProvider)
+        {
+
+            this.originProvider = originProvider;
+        }
+
+
+        public IEnumerable<string> GetTradeData()
+        {
+
+            var tradeData = originProvider.GetTradeData();
+            var changeTradeData = new List<string>();
+           
+
+            foreach (var trade in tradeData)
+            {
+                
+                var adjustedTrade = trade.Replace("GBP", "EUR");
+                changeTradeData.Add(adjustedTrade);
+            }
+
+           
+            return changeTradeData;
+        }
+    }
+}
+        
+    
+
